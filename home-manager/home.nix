c91af2a5437c67a0122a250f559840ca8e90e61f
Lev <lev.perschin@gmail.com>
2024-01-14 23:48:11 +0100

    first version flake config

---
+++ home-manager/home.nix
@@ -0,0 +1,230 @@
+{ pkgs, ... }:
+let editor = "hx";
+in {
+  # You can import other home-manager modules here
+  imports = [
+    # If you want to use home-manager modules from other flakes (such as nix-colors):
+    # inputs.nix-colors.homeManagerModule
+
+    # You can also split up your configuration and import pieces of it here:
+    # ./nvim.nix
+  ];
+
+  nixpkgs = {
+    # You can add overlays here
+    overlays = [
+      # If you want to use overlays exported from other flakes:
+      # neovim-nightly-overlay.overlays.default
+
+      # Or define it inline, for example:
+      # (final: prev: {
+      #   hi = final.hello.overrideAttrs (oldAttrs: {
+      #     patches = [ ./change-hello-to-hi.patch ];
+      #   });
+      # })
+    ];
+    # Configure your nixpkgs instance
+    config = {
+      # Disable if you don't want unfree packages
+      allowUnfree = true;
+      # Workaround for https://github.com/nix-community/home-manager/issues/2942
+      allowUnfreePredicate = _: true;
+    };
+  };
+
+  home = {
+    username = "lev";
+    homeDirectory = "/home/lev";
+    stateVersion = "23.11";
+
+    sessionVariables = {
+      EDITOR = editor;
+      BROWSER = "brave";
+      TERMINAL = "wezterm";
+    };
+
+    shellAliases = {
+      ls = "exa";
+      zl = "zellij";
+      zlf = "zellij --layout ~/.config/zellij/layouts/first.kdl";
+      htop = "btm -b";
+      hms = "home-manager switch";
+      home = "${editor} ~/.config/home-manager/home.nix";
+      nix-conf = "sudoedit /etc/nixos/configuration.nix";
+      nix-bs = "sudo nixos-rebuild --upgrade switch";
+      memory = "${editor} ~/Notes/";
+    };
+
+    packages = with pkgs; [
+
+      appimage-run
+      ardour
+      audacity
+      bandwhich
+      bat
+      blender
+      bottom
+      brave
+      broot
+      deno
+      du-dust
+      eza
+      f3d
+      fd
+      freecad
+      gimp
+      godot_4
+      gitui
+      helix
+      just
+      kitty
+      libreoffice-fresh
+      libsForQt5.kcalc
+      libsForQt5.kdenlive
+      mediainfo
+      megasync
+      nixd
+      nil
+      nixfmt
+      ouch
+      # oterm
+      ripgrep
+      rustup
+      signal-desktop
+      spotify
+      steam
+      tealdeer
+      telegram-desktop
+      thunderbird
+      tokei
+      vlc
+      vscode
+      wezterm
+      zellij
+      zoom-us
+      wl-clipboard
+
+      (pkgs.discord-canary.override { withVencord = true; })
+
+    ];
+
+    file = {
+      ".config/zellij/layouts/first.kdl".text = ''
+        layout {
+            pane split_direction="vertical" {
+                pane
+                pane
+            }
+            pane size=1 borderless=true {
+                plugin location="zellij:compact-bar"
+            }
+        }
+      '';
+
+      ".config/zellij/config.kdl".source = ./zellij/config.kdl;
+
+      ".config/helix/config.toml".text = ''
+        theme = "doom_acario_dark"
+
+        [editor]
+        bufferline = "always"
+
+        [editor.soft-wrap]
+        enable = true
+        max-wrap = 30
+
+        [editor.cursor-shape]
+        insert = "bar"
+        normal = "block"
+
+        [keys.normal]
+        space = { W = ":w"}
+        D = ["ensure_selections_forward", "extend_to_line_end"]
+        esc = ["collapse_selection", "keep_primary_selection"]
+      '';
+
+      ".config/helix/languages.toml".text = ''
+        [[language]]
+        name = "javascript"
+        formatter = { command = 'deno', args = ["fmt", "-", "--ext", "js" ] }
+        auto-format = true
+
+        [[language]]
+        name = "json"
+        formatter = { command = 'deno', args = ["fmt", "-", "--ext", "json" ] }
+
+        [[language]]
+        name = "markdown"
+        formatter = { command = 'deno', args = ["fmt", "-", "--ext", "md" ] }
+        auto-format = true
+
+        [[language]]
+        name = "typescript"
+        formatter = { command = 'deno', args = ["fmt", "-", "--ext", "ts" ] }
+        auto-format = true
+
+        [[language]]
+        name = "jsx"
+        formatter = { command = 'deno', args = ["fmt", "-", "--ext", "jsx" ] }
+        auto-format = true
+
+        [[language]]
+        name = "tsx"
+        formatter = { command = 'deno', args = ["fmt", "-", "--ext", "tsx" ] }
+        auto-format = true
+
+        [[language]]
+        name = "nix"
+        formatter = { command = 'nixfmt', args = ["-w", "100"] }
+        auto-format = true
+      '';
+
+      ".config/kitty/kitty.conf".text = ''
+        font_size 13.0
+        font_family JetBrains Mono
+        include theme.conf
+      '';
+
+      ".config/kitty/theme.conf".source = ./kitty/theme.conf;
+
+      ".wezterm.lua".text = ''
+        local wezterm = require 'wezterm'
+        local config = {}
+
+        if wezterm.config_builder then
+          config = wezterm.config_builder()
+        end
+
+        config.color_scheme = "lovelace"
+        config.font_size = 13.0
+        config.hide_tab_bar_if_only_one_tab = true 
+        config.warn_about_missing_glyphs = false
+
+        return config
+      '';
+
+    };
+  };
+
+  programs = {
+    home-manager.enable = true;
+    direnv.enable = true;
+    starship.enable = true;
+    bash.enable = true;
+    git = {
+      enable = true;
+      lfs.enable = true;
+      userName = "Lev Perschin";
+      userEmail = "lev@perschin.net";
+    };
+  };
+
+  services.gpg-agent = {
+    enable = true;
+    defaultCacheTtl = 1800;
+    enableSshSupport = true;
+  };
+
+  # Nicely reload system units when changing configs
+  systemd.user.startServices = "sd-switch";
+}
---
+++ home-manager/nvim.nix
@@ -0,0 +1,148 @@
+{ pkgs, ... }:
+let nixvim = import (builtins.fetchGit { url = "https://github.com/nix-community/nixvim"; });
+in {
+
+  imports = [ nixvim.homeManagerModules.nixvim ];
+
+  programs.nixvim = {
+    enable = true;
+    globals.mapleader = " ";
+    colorschemes.ayu.enable = true;
+
+    plugins = {
+      lightline.enable = true;
+      lsp = {
+        enable = true;
+        servers = {
+          tsserver.enable = true;
+          rust-analyzer = {
+            enable = true;
+            installCargo = true;
+            installRustc = true;
+          };
+          nixd.enable = true;
+          omnisharp.enable = true;
+        };
+      };
+      ollama.enable = true;
+      nvim-cmp = {
+        enable = true;
+        autoEnableSources = true;
+        sources = [ { name = "nvim_lsp"; } { name = "path"; } { name = "buffer"; } ];
+      };
+      lsp-format.enable = true;
+      lspsaga.enable = true;
+      neogit.enable = true;
+      neorg = {
+        enable = true;
+        modules = {
+          "core.defaults" = { __empty = null; };
+          "core.concealer" = { };
+          "core.integrations.treesitter" = { };
+        };
+      };
+      telescope.enable = true;
+      treesitter.enable = true;
+      barbar.enable = true;
+      luasnip.enable = true;
+      noice.enable = true;
+      which-key.enable = true;
+      mini = {
+        enable = true;
+        modules = {
+          basics = { };
+          ai = {
+            n_lines = 50;
+            search_method = "cover_or_next";
+          };
+          comment = { };
+          surround = { };
+          clue = { };
+          jump = { };
+          move = { };
+          files = { };
+          pairs = { };
+          indentscope = { };
+        };
+      };
+    };
+    options = {
+      number = true;
+      relativenumber = true;
+      shiftwidth = 2;
+    };
+    extraConfigLua = "";
+    keymaps = [
+      {
+        mode = "n";
+        key = "gn";
+        action = "<cmd>BufferNext<CR>";
+      }
+      {
+        mode = "n";
+        key = "gN";
+        action = "<cmd>BufferPrevious<CR>";
+      }
+      {
+        mode = "n";
+        key = "gd";
+        action = "<cmd>Lspsaga goto_definition<CR>";
+      }
+      {
+        mode = "n";
+        key = "<leader>a";
+        action = "<cmd>Lspsaga code_action<CR>";
+      }
+      {
+        mode = "n";
+        key = "<leader>rr";
+        action = "<cmd>Lspsaga rename<CR>";
+      }
+      {
+        mode = "n";
+        key = "<leader>k";
+        action = "<cmd>Lspsaga hover_doc<CR>";
+      }
+      {
+        mode = "n";
+        key = "<leader>K";
+        action = "<cmd>Lspsaga peek_definition<CR>";
+      }
+      {
+        mode = "n";
+        key = "<leader>b";
+        action = "<cmd>Telescope buffers<CR>";
+      }
+      {
+        mode = "n";
+        key = "<leader>f";
+        action = "<cmd>Telescope find_files<CR>";
+      }
+      {
+        mode = "n";
+        key = "<leader>d";
+        action = "<cmd>Telescope diagnostics<CR>";
+      }
+      {
+        mode = "n";
+        key = "<leader>re";
+        action = "<cmd>Telescope lsp_references<CR>";
+      }
+      {
+        mode = "n";
+        key = "<leader>s";
+        action = "<cmd>Telescope lsp_document_symbols<CR>";
+      }
+      {
+        mode = "n";
+        key = "<leader>o";
+        action = "<cmd>Telescope outline<CR>";
+      }
+      {
+        mode = "i";
+        key = "<cr>z";
+        action = "<cmd>Telescope outline<CR>";
+      }
+    ];
+  };
+}
